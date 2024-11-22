6eff0ae155718246ca29fbb98c49bb51d46aba9c
hrish1812 <hrishiudawant7@gmail.com>
2024-11-22 11:02:43 +0530

    Update 169.MajorityElement.c

--- 169.MajorityElement.c
+++ 169.MajorityElement.c
@@ -1,1 +1,29 @@
-
+int majorityElement(int* nums, int numsSize) {
+    int vote = 0;
+    int candidates = -1;
+    for(int i=0;i<numsSize;i++){
+        if(vote == 0){
+            candidates=nums[i];
+            vote=1;
+        }
+        else{
+            if(candidates == nums[i]){
+                vote++;
+            }
+            else{
+                vote--;
+            }
+        }
+    }
+        int count=0;
+        for(int i = 0; i < numsSize ; i++){
+            if(nums[i]==candidates){
+                count++;
+            }
+        }
+            if(count>numsSize/2)
+                return candidates;
+            
+            else
+                return -1;       
+}
